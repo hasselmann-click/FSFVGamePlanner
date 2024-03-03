8630e23d741cbd2cdaf45e8b6a1a618fce1c9a97
Radall <bj13_90@hotmail.com>
2024-03-03 12:24:59 +0100

    tbd

--- FSFV.Gameplanner.Appworks/AppworksTransformer.cs
+++ FSFV.Gameplanner.Appworks/AppworksTransformer.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FSFV.Gameplanner.Appworks.Mappings;
+namespace FSFV.Gameplanner.Appworks;
 public class AppworksTransformer
 {
 
--- FSFV.Gameplanner.Appworks/Class1.cs
+++ FSFV.Gameplanner.Appworks/Class1.cs
@@ -9,14 +9,13 @@ public class AppworksImportRecord
 {
 
     public static readonly string DateFormat = "dd/mm/yyyy HH:ii";
-    private DateTime start1;
 
     public int matchday_id { get; set; }
     public int division_id { get; set; }
     public int location_id { get; set; }
     public int home_team_id { get; set; }
     public int away_team_id { get; set; }
-    public string start { get => start1.ToString(DateFormat); set => start1 = value; }
+    public DateTime start { get; set; }
     public int referee_team_id { get; set; }
 
 }
