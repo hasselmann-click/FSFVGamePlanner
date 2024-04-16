2cf27a59e38c6adb99da383346057b248827b075
Radall <bj13_90@hotmail.com>
2024-04-16 17:42:30 +0200

    added pdf project

---
+++ FSFV.Gameplanner.Pdf/Runner.cs
@@ -0,0 +1,11 @@
+﻿using FSFV.Gameplanner.Service.Serialization;
+
+namespace FSFV.Gameplanner.Pdf;
+
+public class PdfGenerator(FsfvCustomSerializerService serializer)
+{
+    public async Task GenerateAsync(Func<Task<Stream>> gameplanCsvStream)
+    {
+        var games = await serializer.ParseGameplanAsync(gameplanCsvStream);
+    }
+}
