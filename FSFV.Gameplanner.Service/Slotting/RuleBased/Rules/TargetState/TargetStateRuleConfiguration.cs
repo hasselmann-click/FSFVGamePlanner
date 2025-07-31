acb0f1aa7c334fc7d7288615cb9819a9255cfec4
radall <radall@outlook.com>
2025-07-31 10:43:07 +0200

    improved logging. Fixes target state rule start time handling

--- FSFV.Gameplanner.Service/Slotting/RuleBased/Rules/TargetState/TargetStateRuleConfiguration.cs
+++ FSFV.Gameplanner.Service/Slotting/RuleBased/Rules/TargetState/TargetStateRuleConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CsvHelper.Configuration.Attributes;
+using System;
 
 namespace FSFV.Gameplanner.Service.Slotting.RuleBased.Rules.TargetState;
 
@@ -7,7 +8,23 @@ public class TargetStateRuleConfiguration
     public Current Filter { get; set; }
     public Target Applicator { get; set; }
 
-    public record Current(int? Round, TimeOnly? Time, DateOnly? Date, string? Pitch);
+    public record Current
+    {
+        public int? Round { get; init; }
+        public TimeOnly? Time { get; init; }
+        [Format("dd.MM.yyyy", "yyyy.MM.dd")]
+        public DateOnly? Date { get; init; }
+        public string? Pitch { get; init; }
+
+        public void Deconstruct(out int? round, out TimeOnly? time, out DateOnly? date, out string? pitch)
+        {
+            round = Round;
+            time = Time;
+            date = Date;
+            pitch = Pitch;
+        }
+    }
+
     public record Target(string? Team, TimeOnly? Time, string? League);
 
 }
