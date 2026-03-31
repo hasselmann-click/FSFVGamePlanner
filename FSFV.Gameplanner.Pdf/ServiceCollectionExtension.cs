a85ecbef8b7924c2580438828fa40c4308162a54
Radall <bj13_90@hotmail.com>
2026-03-31 18:38:26 +0200

    enable pdf services

--- FSFV.Gameplanner.Pdf/ServiceCollectionExtension.cs
+++ FSFV.Gameplanner.Pdf/ServiceCollectionExtension.cs
@@ -1,6 +1,7 @@
 using System.Drawing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using FSFV.Gameplanner.Service.Serialization;
 
 namespace FSFV.Gameplanner.Pdf;
 
@@ -8,10 +9,12 @@ public static class ServiceCollectionExtension
 {
     public static IServiceCollection AddPdfServices(this IServiceCollection serviceCollection, IConfiguration configuration)
     {
+        PdfConfig? pdfConfig = configuration.GetSection("PdfConfig").Get<PdfConfig>();
+        if (pdfConfig is null)
+        {
+            throw new InvalidOperationException("Missing required PdfConfig configuration section.");
+        }
 
-        throw new NotImplementedException("We need a solution for generating PDFs on a linux server first");
-
-        var pdfConfig = configuration.GetSection("PdfConfig").Get<PdfConfig>();
         // explicitly convert the dictionary of strings to dictionary of colors
         // since IConfigurationSections doesn't use custom json converters
         var pdfConigLeagueColors = configuration.GetSection("PdfConfig:LeagueColors").Get<Dictionary<string, string>>();
@@ -20,6 +23,7 @@ public static class ServiceCollectionExtension
         // pdfConfig.LeagueColors = pdfConigLeagueColors.ToDictionary(x => x.Key, x => Color.FromHex(x.Value));
 
         serviceCollection
+            .AddTransient<CsvSerializerService>()
             .AddSingleton(pdfConfig)
             .AddTransient<PdfGenerator>();
 
